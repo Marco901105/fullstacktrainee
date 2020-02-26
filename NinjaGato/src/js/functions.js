f2014b930bfc8eebbd6f8e520199c13faf67ddf2
Enigma911 <55920341+Marco901105@users.noreply.github.com>
2020-02-26 17:29:03 -0300

    puzzle gato ninja

---
+++ NinjaGato/src/js/functions.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.hide-img').hide();
+
+    var puzzle = $('.img-fluid').map(function() {
+        return $(this).attr('data-alt-src')
+    }).get();
+
+    $('.img-fluid').click(function() {
+        var num = Math.floor(Math.random() * puzzle.length);
+        data = $(this).attr('data-alt-src', puzzle[num]);
+        url = $(this).attr('src', puzzle[num]);
+    });
+});
